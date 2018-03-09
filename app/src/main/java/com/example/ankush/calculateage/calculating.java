ab9584aca6261a09e52cf67660c3c379ad7b9d1f
rawkush <ankushrawat950@gmail.com>
2018-03-09 15:46:43 +0530

    checking validity of current date

--- app/src/main/java/com/example/ankush/calculateage/calculating.java
+++ app/src/main/java/com/example/ankush/calculateage/calculating.java
@@ -24,8 +24,51 @@ public  calculating(int dd,int mm,int yy,int cdd,int cmm,int cyy){
 }
 
 
-public static String find(){
+public  String find(){
 
+    if(y<=cy&&cy>0&&y>0)
+    {
+        if(m==1||m==3||m==5||m==7||m==8||m==10||m==12)
+        {
+            if(d>0&&d<=31)
+            {
+               // "date is valid\n";
+                val=1;
+            }
+        }else
+        if(m==4||m==6||m==9||m==11)
+        {
+            if(d>0&&d<=30)
+            {
+                //<"date is valid\n";
+                val=1;
+            }
+        }
+
+        if(y%4==0)
+        {
+            lp=1;	if(m==2)
+        {
+            if(d>0&&d<=29)
+            {
+                //" date is valid\n";
+                val=1;
+            }
+        }
+        }
+
+        if(y%4!=0)
+        {
+            if(m==2)
+            {	if(d>0&&d<=28)
+            {
+                //" date is valid \n";
+                val=1;
+            }
+            }
+        }
+
+    }
 
 
 
