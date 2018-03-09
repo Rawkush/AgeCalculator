f571edc9d506d1b16661f705a53c11e31982c8aa
rawkush <ankushrawat950@gmail.com>
2018-03-09 15:42:01 +0530

    calculating class

--- app/src/main/java/com/example/ankush/calculateage/calculating.java
+++ app/src/main/java/com/example/ankush/calculateage/calculating.java
@@ -5,4 +5,34 @@ package com.example.ankush.calculateage;
  */
 
 public class calculating {
+
+
+private    int d=0,m=0,y=0,val=0,cval=0;
+private     int cd=0,cm=0,cy=0;
+private     int ty=0,tm=0,td=0;
+private     int lp=0;
+
+public  calculating(int dd,int mm,int yy,int cdd,int cmm,int cyy){
+
+    d=dd;
+    m=mm;
+    y=yy;
+    cd=cdd;
+    cm=cmm;
+    cy=cyy;
+
+}
+
+
+public static String find(){
+
+
+
+
+
+
+    }
+
+
+
 }
