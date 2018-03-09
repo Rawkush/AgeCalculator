e02a4eedfe8affb686d5805c0e33b13e38e58647
rawkush <ankushrawat950@gmail.com>
2018-03-09 15:35:32 +0530

    reading inputs

--- app/src/main/java/com/example/ankush/calculateage/MainActivity.java
+++ app/src/main/java/com/example/ankush/calculateage/MainActivity.java
@@ -1,33 +1,68 @@
 package com.example.ankush.calculateage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-private TextView textView;
-private Button button;
+
+//private Button button;
 private EditText cdate;
-private EditText dob;
+private EditText date;
+private EditText year;
+private EditText cyear;
+private EditText month;
+private EditText cmonth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView =(TextView) findViewById(R.id.result);
-        button =(Button) findViewById(R.id.button);
+        //button =(Button) findViewById(R.id.button);
         cdate =(EditText) findViewById(R.id.cdate);
-        dob = (EditText) findViewById(R.id.dob);
+        date = (EditText) findViewById(R.id.date);
+        year =(EditText) findViewById(R.id.year);
+        cyear =(EditText) findViewById(R.id.cyear);
+        month =(EditText) findViewById(R.id.month);
+        cmonth =(EditText) findViewById(R.id.cmonth);
 
     }
 
 
     public void calculateAge(View view) {
 
-        
+
+        int d=0,m=0,y=0;
+        int cd=0,cm=0,cy=0;
+
+        String s=null;
+        s=date.getText().toString();
+        d= Integer.parseInt(s);
+
+        s=month.getText().toString();
+        m= Integer.parseInt(s);
+
+        s=year.getText().toString();
+        y= Integer.parseInt(s);
+
+        s=cdate.getText().toString();
+        cd= Integer.parseInt(s);
+
+        s=cmonth.getText().toString();
+        cm= Integer.parseInt(s);
+
+        s=cyear.getText().toString();
+        cy= Integer.parseInt(s);
+
+
+
+
+
     }
 
 
---
+++ app/src/main/java/com/example/ankush/calculateage/calculating.java
@@ -0,0 +1,8 @@
+package com.example.ankush.calculateage;
+
+/**
+ * Created by Ankush on 3/9/2018.
+ */
+
+public class calculating {
+}
